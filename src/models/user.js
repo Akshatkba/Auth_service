4cafc2abc834fb4d61f2b8285ce66f15e079d30d
Akshat Bansal <akshatkba24@gmail.com>
2024-06-26 23:00:56 +0530

    added roles and user_roles through tables

--- src/models/user.js
+++ src/models/user.js
@@ -14,6 +14,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.belongsToMany(models.Role,{
+        through: 'User_Roles'
+      }
+      )
     }
   }
   User.init({
